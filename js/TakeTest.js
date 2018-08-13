41680b18d82584b5060dab49fb5183b69a867be4
VishalGirishankar <career.vishalshankar@gmail.com>
2018-08-13 19:14:29 +0530

    MDB

--- js/TakeTest.js
+++ js/TakeTest.js
@@ -11,11 +11,23 @@ $(document).ready(function(){
     $('#TakeTest').click(function (e) { 
         e.preventDefault();
     $(this).remove();
-    $('McqHeader').show('qqq');
-    $('McqContent').show();
-    $('StartTest').show();
-    $("McqHeader").html(Instruction[0].InstructionHeader);
-    $("McqContent").html(Instruction[1].InstructionSet[1]);
+    $('#McqHeader').show();
+    $('#McqContent').show();
+    $('#StartTest').show();
+    $("#McqHeader").html(Instruction[0].InstructionHeader);
+    let length=Instruction[1].InstructionSet.length
+    alert(length)
+    for(let q=0;q<=length;q++)
+    {
+    $("#McqContent").append(Instruction[1].InstructionSet[q]);
+    }
     });
+
+    $("#StartTest").click(function (e) { 
+        e.preventDefault();
+        
+    });
+
+
 
 });
